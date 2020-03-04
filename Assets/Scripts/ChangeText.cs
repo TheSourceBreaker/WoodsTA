f8f705189aae89e1bf737bc9aee58ccc13b05502
Jahvier <50281954+TheSourceBreaker@users.noreply.github.com>
2020-03-04 12:50:53 -0800

    Update

---
+++ Assets/Scripts/ChangeText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    public Text changingText;
+
+    public GameObject changingText2;
+
+    public void TextChange()
+    {
+        changingText.text = "2";
+        changingText2.GetComponent<Text>().text = "2";
+    }
+}
---
+++ Assets/Scripts/Story.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Story : MonoBehaviour
+{
+    public int playerHealth;
+    public string currentRoute;
+    public string nextRoute;
+    public int area;
+
+    decision normalRoute;
+    decision saveRoute;
+    decision deathRoute;
+    
+    void Update()
+    {
+        //loop through decision maker
+        if (playerHealth == 0)
+        {
+            //player died
+        }
+    }
+}
+
+
+public interface decision
+{
+    decision makeDecision();
+}
+
+public class NormalWay
+{
+    int value;
+    decision normalRoute;
+    decision saveRoute;
+    decision deathRoute;
+
+    public NormalWay(decision normalRoute, decision saveRoute, decision deathRoute)
+    {
+        //if player chooses a route that is under the category "NormalWay"
+        //value = 1 and normalRoute = true
+        //else if player chooses a route that is under the category "SaveRoute"
+        //value = 2 and saveRoute = true
+        //else if player chooses a route that is under the category "DeathRoute"
+        //value = 3 and deathRoute = true
+    }
+
+    decision makeDecision()
+    {
+        //value = 1 and normalRoute = true
+        //value = 3 and saveRoute = true
+        //value = 2 and deathRoute = true
+        return null;
+    }
+}
+
+public class SavePoint
+{
+    Story story;
+
+    public SavePoint(Story _story)
+    {
+        story = _story;
+    }
+
+    decision makeDecision()
+    {
+        //override existing text file with new data
+        System.IO.StreamWriter file = new System.IO.StreamWriter(@"woods");
+
+
+
+        return null;
+    }
+}
+
+public class DeathWay
+{
+    Story story;
+
+    public DeathWay(Story _story)
+    {
+        story = _story;
+    }
+    decision makeDecision()
+    {
+        //this would check the route and area, then give out the appropriate death
+        return null;
+    }
+}
